4320d03620a1d46c45748e06efdaa11fdf8ae758
i8mb <125501232+i8mb@users.noreply.github.com>
2023-12-20 17:57:37 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
-
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+	x.className += " responsive";
+} 
+else {
+		x.className = "topnav";
+	}
+}
+document.querySelector(".topnav a.active").click();
+document.querySelectorAll(".topnav a").forEach(function(link) {
+  link.addEventListener("click", function(event) {
+		event.preventDefault();
+		document.querySelectorAll(".topnav a").forEach(function(link) {
+		  link.classList.remove("active");
+		});
+		event.target.classList.add("active");
+		document.querySelectorAll("div[id]").forEach(function(div) {
+		  div.style.display = "none";
+		});
+		document.querySelector(event.target.getAttribute("href")).style.display = "block";
+		});
+});
