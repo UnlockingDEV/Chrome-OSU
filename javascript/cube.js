207b2580412ea034748d2eb35bc8b9724384259b
i8mb <125501232+i8mb@users.noreply.github.com>
2023-12-20 18:37:01 -0500

    Update cube.js

--- javascript/cube.js
+++ javascript/cube.js
@@ -1,1 +1,18 @@
+var container = document.querySelector('.container');
+var cube = document.querySelector('.cube');
 
+container.addEventListener('mousemove', function(e) {
+  var x = e.clientX / window.innerWidth;
+  var y = e.clientY / window.innerHeight;
+  
+  cube.style.transform = 'rotateX(' + (y * 20) + 'deg) rotateY(' + (x * 20) + 'deg)';
+});
+
+var observer = new IntersectionObserver(function(entries) {
+  if (entries[0].isIntersecting === false) {
+    cube.style.left = '-50px';
+    cube.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
+  }
+});
+
+observer.observe(container);
